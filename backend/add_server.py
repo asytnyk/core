67c3f0e0d2295913261e7d01b18c2777f331d3d7
Peter Senna Tschudin <peter.senna@gmail.com>
2018-03-08 10:50:58 +0100

    add_server.py: initial commit

--- backend/add_server.py
+++ backend/add_server.py
@@ -15,27 +15,37 @@ SERVER_ADD_QUERY = """
     values (?,?,?,?,?)
 """
 
-def main():
+def add_server(customer_uuid, activation_uuid, ssh_uuid, vpn_uuid):
+
     server_uuid = str(uuid.uuid4())
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--activation', type=str)
-    parser.add_argument('--customer', type=str)
-    parser.add_argument('--ssh', type=str)
-    parser.add_argument('--vpn', type=str)
-    args = parser.parse_args()
-
-    if not args.activation or not args.customer or not args.ssh or not args.vpn:
-        print "Error, some argument(s) missing"
-        sys.exit()
+    if not server_uuid or not customer_uuid or not activation_uuid or not ssh_uuid or not vpn_uuid:
+        raise ValueError('Argument(s) missing', server_uuid, customer_uuid, activation_uuid,
+                ssh_uuid, vpn_uuid)
 
     c = db.cursor()
     c.execute("PRAGMA foreign_keys=ON");
-    c.execute(SERVER_ADD_QUERY, (server_uuid, args.customer, args.activation,
-        args.ssh, args.vpn))
+    c.execute(SERVER_ADD_QUERY, (server_uuid, customer_uuid, activation_uuid, ssh_uuid, vpn_uuid))
 
     db.commit()
     db.close()
+
+    return server_uuid
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--activation', type=str, help='activation uuid')
+    parser.add_argument('--customer', type=str, help='customer uuid')
+    parser.add_argument('--ssh', type=str, help='ssh_key uuid')
+    parser.add_argument('--vpn', type=str, help='vpn_key uuid')
+    args = parser.parse_args()
+
+    if not args.activation or not args.customer or not args.ssh or not args.vpn:
+        raise ValueError('Argument(s) missing', args.activation, args.customer, args.ssh, args.vpn)
+
+    server_uuid = add_server(args.customer, args.activation, args.ssh, args.vpn)
+
+    print "server_uuid: " + server_uuid
 
 if __name__ == "__main__":
     main()
