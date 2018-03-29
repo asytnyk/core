0000c35be45315d3e1feae58ff71b41e5f82ad64
Peter Senna Tschudin <peter.senna@gmail.com>
2018-03-29 13:41:26 +0200

    minor changes to the server page

--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -274,7 +274,7 @@ def create_it_all(user, facter_json, session):
             manufacturer_id = manufacturer.id,
             productname_id = productname.id,
             processor_id = processor0.id,
-            facter_json = json.dumps(facter_json, sort_keys = True, indent = 4, separators = (',', ': ')))
+            facter_json = json.dumps(facter_json))
 
     facts.add_macaddress(macaddress)
 
@@ -517,7 +517,8 @@ def server(uuid):
         flash('Invalid Server')
         return redirect(url_for('list_servers'))
 
-    facter = server.get_facts().facter_json
+    facter = json.dumps(json.loads(server.get_facts().facter_json), sort_keys = True, indent = 4,
+            separators = (',', ': '))
 
     return render_template('server.html', title='Your server',
             servers=[server,], facter=facter)
