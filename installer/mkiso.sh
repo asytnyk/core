2544635c1d9918a99a6266cabbd4d49e7bcfde72
Peter Senna Tschudin <peter.senna@gmail.com>
2018-03-30 23:08:31 +0200

    doing awesome stuff like adding a new partition for the user to save the installer key

--- installer/mkiso.sh
+++ installer/mkiso.sh
@@ -6,8 +6,10 @@ url="http://mirror.switch.ch/ftp/mirror/fedora/linux/releases/27/Server/x86_64/i
 builddir="$PWD/builder"
 
 volumename='Fedora-S-dvd-x86_64-27' #xorriso -indev <iso file> 2>&1 | grep 'Volume id'
-
 mntdir='/mnt/linux'
+
+# New partition is created for the user to save the installation key
+extramegs=5
 
 if [[ ! -f $infilename ]]; then
 	wget $url
@@ -25,9 +27,14 @@ if [[ $? != 0 ]]; then
 fi
 
 cd /mnt
+rm -rf $builddir/*
 tar -cvf - linux | (cd $builddir && tar -xf - )
 
+cd $builddir/..
+rsync -vauz toadd/ $builddir/
+
 cd $builddir/linux
+
 
 xorriso -as mkisofs\
 	-R\
@@ -53,3 +60,33 @@ rm -f $outfilename
 mv /tmp/$outfilename .
 
 sudo umount $mntdir
+
+# add a partition for the user to save the installation key
+dd if=/dev/zero of=$outfilename bs=1M count=$extramegs oflag=append conv=notrunc
+
+# From: https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
+# to create the partitions programatically (rather than manually)
+# we're going to simulate the manual input to fdisk
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $outfilename
+  n # new partition
+  p # primary partition
+  4 # Partition number
+    # default - start at beginning of disk 
+    # default - end at the end of disk
+  t # change partitio type
+  4 # of partition 4
+  c # to fat
+  w # write the partition table
+EOF
+
+loopdev=$(sudo kpartx -av $outfilename|grep p4|cut -d ' ' -f 3)
+
+sudo mkfs.vfat -n 'iWe' /dev/mapper/$loopdev
+
+sudo kpartx -dv $outfilename
+
+
