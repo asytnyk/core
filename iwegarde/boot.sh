96f69cc33ae74a4b82c27664bc4b9c6f3ede791a
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-02 14:39:56 +0200

    iwegard -> iwegarde

--- iwegarde/boot.sh
+++ iwegarde/boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - iwegard:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - iwegarde:app
